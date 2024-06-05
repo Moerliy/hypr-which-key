4b3e762e60a4a75ee0db3bd4b83f19a5ef0683d7
Moritz Gleissner <moritz@gleissner.de>
2024-06-05 16:22:25 +0200

    base main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <hyprland/src/plugins/PluginAPI.hpp>
 
-inline HANDLE PHANDLE = nullptr;
+inline HANDLE                          PHANDLE = nullptr;
 
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     PHANDLE = handle;
@@ -10,14 +10,13 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     // ALWAYS add this to your plugins. It will prevent random crashes coming from
     // mismatched header versions.
     if (HASH != GIT_COMMIT_HASH) {
-        HyprlandAPI::addNotification(PHANDLE, "[MyPlugin] Mismatched headers! Can't proceed.",
-                                     CColor{1.0, 0.2, 0.2, 1.0}, 5000);
-        throw std::runtime_error("[MyPlugin] Version mismatch");
+        HyprlandAPI::addNotification(PHANDLE, "[Hypr-Which-Key] Mismatched headers! Can't proceed.", CColor{1.0, 0.2, 0.2, 1.0}, 5000);
+        throw std::runtime_error("[Hypr-Which-key] Version mismatch");
     }
 
     // ...
 
-    return {"MyPlugin", "An amazing plugin that is going to change the world!", "Me", "1.0"};
+    return {"Hypr-Which-Key", "An amazing plugin that is going to change the world!", "Moritz Gleissner, Yusuf Duran", "0.1"};
 }
 
 APICALL EXPORT void PLUGIN_EXIT() {
