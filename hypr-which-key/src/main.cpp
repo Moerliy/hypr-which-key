c054d7f4ec2e7f55f73a06eb9a62b031902499c6
Moritz Gleissner <moritz@gleissner.de>
2024-06-07 18:55:05 +0200

    added submap event listener

--- hypr-which-key/src/main.cpp
+++ hypr-which-key/src/main.cpp
@@ -6,12 +6,20 @@
 
 #include <hyprland/src/plugins/PluginAPI.hpp>
 #include <hyprland/src/managers/KeybindManager.hpp>
+#include <hyprland/src/config/ConfigManager.hpp>
 
 #include "globals.hpp"
 
 // Do NOT change this function.
 APICALL EXPORT std::string PLUGIN_API_VERSION() {
     return HYPRLAND_API_VERSION;
+}
+
+static void onSubmap(void* self, std::any data) {
+    // data is guaranteed
+    const auto submap = std::any_cast<std::string>(data);
+
+    HyprlandAPI::addNotification(PHANDLE, "[hypr-which-key] changed submap to " + submap + " !", CColor{0.2, 1.0, 0.2, 1.0}, 5000);
 }
 
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
@@ -26,11 +34,19 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
         throw std::runtime_error("[hypr-which-key] Version mismatch");
     }
 
+    const auto&        COMMAND    = "bind";
+    const auto&        VALUE      = "SUPER, V, \"Test\", exec, kitty";
+    const auto&        keybindRet = g_pConfigManager->handleBind(COMMAND, VALUE);
+
     std::ostringstream printStr;
     for (const auto& bind : g_pKeybindManager->m_lKeybinds) {
-        printStr << "submap: '" << bind.submap << "' on key: '" << bind.key << "'\n";
+        if (bind.submap != "") {
+            continue;
+        }
+        printStr << "submap: '" << bind.submap << "' on key: '" << bind.key << "', arg: '" << bind.arg << "', handler: '" << bind.handler << "', desc: '" << bind.description
+                 << "'\n";
     }
-
+    static auto P = HyprlandAPI::registerCallbackDynamic(PHANDLE, "submap", [&](void* self, SCallbackInfo& info, std::any data) { onSubmap(self, data); });
     // throw std::runtime_error(printStr.str());
 
     HyprlandAPI::addNotification(PHANDLE, "[hypr-which-key] Initialized successfully!", CColor{0.2, 1.0, 0.2, 1.0}, 5000);
