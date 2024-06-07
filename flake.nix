e24f8be1c5d8bb234f86d4a1f7cfa1a693b8a657
Moritz Gleissner <moritz@gleissner.de>
2024-06-07 15:19:00 +0200

    change hyprland to fork for tests

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "hypr-which-key";
 
   inputs = {
-    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    hyprland.url = "git+https://github.com/Moerliy/Hyprland?submodules=1";
     systems.follows = "hyprland/systems";
   };
 
